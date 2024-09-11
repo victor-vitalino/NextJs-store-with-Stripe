70f0271f0d853484f05587f0c97e3d67563c5957
Victor Vitalino <victorvitalino237@gmail.com>
2024-09-11 20:56:57 -0300

    :construction: feat(PRODUCTS): connect with stripe and get products data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,77 @@
 import React from "react";
+import { GetServerSideProps } from "next";
 import Image from "next/image";
 import { HomeContainer, ProductItem } from "@/styles/pages/home";
 
+import { useKeenSlider } from "keen-slider/react";
+
 import guitar1 from "@/assets/guitars/1.png";
 import guitar2 from "@/assets/guitars/2.png";
+import guitar3 from "@/assets/guitars/3.png";
+
+import "keen-slider/keen-slider.min.css";
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
 
 // import { Container } from './styles';
 
-const Home: React.FC = () => {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    url: string;
+    price: number;
+  }[];
+}
+
+const Home: React.FC = ({ products }: HomeProps) => {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <ProductItem>
-        <Image src={guitar1} height={550}  alt="" />
-        <footer>
-          <strong>Guitarra X</strong>
-          <span>R$ 79,99</span>
-        </footer>
-      </ProductItem>
-      <ProductItem>
-        <Image src={guitar2} height={550}  alt="" />
-        <footer>
-          <strong>Guitarra X</strong>
-          <span>R$ 79,99</span>
-        </footer>
-      </ProductItem>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map((i) => (
+        <ProductItem key={i.id} className="keen-slider__slide">
+          <Image
+            src={i.imageUrl}
+            height={550}
+            width={250}
+            style={{ objectFit: "contain" }}
+            alt=""
+          />
+          <footer>
+            <strong>{i.name}</strong>
+            <span>R$ {i.price}</span>
+          </footer>
+        </ProductItem>
+      ))}
     </HomeContainer>
   );
 };
-
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      url: product.url,
+      price: price.unit_amount / 100,
+    };
+  });
+  return {
+    props: { products },
+  };
+};
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -3,7 +3,7 @@ import { styled } from "..";
 
 export const HomeContainer = styled("main", {
   display: "flex",
-  gap: "3rem",
+  // gap: "3rem",
   width: "100%",
   maxWidth: "calc(100vw - ((100vw - 1180px) / 2))",
   marginLeft: "auto",
@@ -14,7 +14,7 @@ export const ProductItem = styled("a", {
   display: "flex",
   background: "linear-gradient(180deg,#1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
-  padding: "0.25rem",
+  // padding: "0.25rem",
   cursor: "pointer",
   position: "relative",
   alignItems: "center",
